06f1d4ec5bbeccdb055bd304c73098a4b2f22074
sduplantis18 <sduplantis18@gmail.com>
2020-07-15 21:03:03 -0400

    Branched Code

--- starter_code/env/Scripts/activate.bat
+++ starter_code/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sdupl\FSND\projects\03_coffee_shop_full_stack\starter_code\env
+set VIRTUAL_ENV=C:\Users\sdupl\03_coffee_shop_full_stack\starter_code\env
 
 if not defined PROMPT set PROMPT=$P$G
 
